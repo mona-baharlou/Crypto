6027d1479736fa759438a6f98d913733d2f0fb27
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-25 15:00:08 +0330

    statistics code updated

--- app/src/main/java/com/baharlou/crypto/features/CoinActivity.kt
+++ app/src/main/java/com/baharlou/crypto/features/CoinActivity.kt
@@ -1,5 +1,6 @@
 package com.baharlou.crypto.features
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.baharlou.crypto.apiManager.model.CoinsData
@@ -8,12 +9,16 @@ import com.baharlou.crypto.databinding.ActivityCoinBinding
 class CoinActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoinBinding
+    private lateinit var dataCoin: CoinsData.Data
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityCoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dataCoin = intent.getParcelableExtra<CoinsData.Data>("dataToSend")!!
+        binding.toolbar.toolbar.title = dataCoin.coinInfo.name
 
         initUI()
     }
@@ -30,18 +35,17 @@ class CoinActivity : AppCompatActivity() {
 
     private fun initStatistics() {
 
-        val dataCoin = intent.getParcelableExtra<CoinsData.Data>("dataToSend")!!
 
-        binding.moduleStatistics.tvOpenAmount.text = "${dataCoin.dISPLAY.uSD.oPEN24HOUR}"
-        binding.moduleStatistics.tvTodayHigh.text = "${dataCoin.dISPLAY.uSD.hIGH24HOUR}"
-        binding.moduleStatistics.tvTodayLow.text = "${dataCoin.dISPLAY.uSD.lOW24HOUR}"
-        binding.moduleStatistics.tvTodayChange.text = "${dataCoin.dISPLAY.uSD.cHANGE24HOUR}"
-        binding.moduleStatistics.tvVolume.text = "${dataCoin.dISPLAY.uSD.vOLUME24HOUR}"
+        binding.moduleStatistics.tvOpenAmount.text = dataCoin.dISPLAY.uSD.oPEN24HOUR
+        binding.moduleStatistics.tvTodayHigh.text = dataCoin.dISPLAY.uSD.hIGH24HOUR
+        binding.moduleStatistics.tvTodayLow.text = dataCoin.dISPLAY.uSD.lOW24HOUR
+        binding.moduleStatistics.tvTodayChange.text = dataCoin.dISPLAY.uSD.cHANGE24HOUR
+        binding.moduleStatistics.tvVolume.text = dataCoin.dISPLAY.uSD.vOLUME24HOUR
 
-        binding.moduleStatistics.tvVolume.text = "${dataCoin.dISPLAY.uSD.vOLUME24HOUR}"
-        binding.moduleStatistics.tvAverageVolume.text = "${dataCoin.dISPLAY.uSD.vOLUMEDAYTO}"
-        binding.moduleStatistics.tvMarketCap.text = "${dataCoin.dISPLAY.uSD.mARKET}"
-        binding.moduleStatistics.tvSupply.text = "${dataCoin.dISPLAY.uSD.sUPPLY}"
+        binding.moduleStatistics.tvVolume.text = dataCoin.dISPLAY.uSD.vOLUME24HOUR
+        binding.moduleStatistics.tvtotalVolume.text = dataCoin.dISPLAY.uSD.tOTALVOLUME24H
+        binding.moduleStatistics.tvMarketCap.text = dataCoin.dISPLAY.uSD.mKTCAP
+        binding.moduleStatistics.tvSupply.text = dataCoin.dISPLAY.uSD.sUPPLY
     }
 
     private fun initChart() {
