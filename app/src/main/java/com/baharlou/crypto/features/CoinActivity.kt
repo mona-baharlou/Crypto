1acfc5b693fc3607c10cfc0190146970af057a31
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-25 14:50:58 +0330

    statistics code added

--- app/src/main/java/com/baharlou/crypto/features/CoinActivity.kt
+++ app/src/main/java/com/baharlou/crypto/features/CoinActivity.kt
@@ -2,6 +2,7 @@ package com.baharlou.crypto.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baharlou.crypto.apiManager.model.CoinsData
 import com.baharlou.crypto.databinding.ActivityCoinBinding
 
 class CoinActivity : AppCompatActivity() {
@@ -13,6 +14,37 @@ class CoinActivity : AppCompatActivity() {
 
         binding = ActivityCoinBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initChart()
+        initStatistics()
+        initAbout()
+    }
+
+    private fun initAbout() {
+
+    }
+
+    private fun initStatistics() {
+
+        val dataCoin = intent.getParcelableExtra<CoinsData.Data>("dataToSend")!!
+
+        binding.moduleStatistics.tvOpenAmount.text = "${dataCoin.dISPLAY.uSD.oPEN24HOUR}"
+        binding.moduleStatistics.tvTodayHigh.text = "${dataCoin.dISPLAY.uSD.hIGH24HOUR}"
+        binding.moduleStatistics.tvTodayLow.text = "${dataCoin.dISPLAY.uSD.lOW24HOUR}"
+        binding.moduleStatistics.tvTodayChange.text = "${dataCoin.dISPLAY.uSD.cHANGE24HOUR}"
+        binding.moduleStatistics.tvVolume.text = "${dataCoin.dISPLAY.uSD.vOLUME24HOUR}"
+
+        binding.moduleStatistics.tvVolume.text = "${dataCoin.dISPLAY.uSD.vOLUME24HOUR}"
+        binding.moduleStatistics.tvAverageVolume.text = "${dataCoin.dISPLAY.uSD.vOLUMEDAYTO}"
+        binding.moduleStatistics.tvMarketCap.text = "${dataCoin.dISPLAY.uSD.mARKET}"
+        binding.moduleStatistics.tvSupply.text = "${dataCoin.dISPLAY.uSD.sUPPLY}"
+    }
+
+    private fun initChart() {
 
     }
 }
