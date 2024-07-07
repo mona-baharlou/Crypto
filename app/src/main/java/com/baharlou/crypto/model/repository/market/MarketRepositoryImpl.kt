8d9c5a64835e7de7c3c23f1da1b8e9f03bc6d834
mona-baharlou <mona.baharlou755@gmail.com>
2024-07-07 11:38:06 +0330

    market repository is updated to implement getNews and getCoins

--- app/src/main/java/com/baharlou/crypto/model/repository/market/MarketRepositoryImpl.kt
+++ app/src/main/java/com/baharlou/crypto/model/repository/market/MarketRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.baharlou.crypto.model.repository.market
 
+import com.baharlou.crypto.model.ApiManager.ApiCallback
 import com.baharlou.crypto.model.data.CoinsData
 import com.baharlou.crypto.model.data.NewsData
 import com.baharlou.crypto.model.net.ApiService
@@ -9,20 +10,18 @@ import retrofit2.Response
 import javax.inject.Inject
 
 class MarketRepositoryImpl(
-    @Inject private val apiService: ApiService
+    @Inject private val apiService: ApiService,
 ) : MarketRepository {
     override suspend fun getNews(): ArrayList<Pair<String, String>> {
-        val data = apiService.getTopNews()
         val dataToSend: ArrayList<Pair<String, String>> = arrayListOf()
-        data.data.forEach {
-            dataToSend.add(Pair(it.title, it.url))
-        }
-        apiCallback.onSuccess(dataToSend)
 
+        val newsData = apiService.getTopNews()
 
+        return dataToSend
+        //if(newsData.)
     }
 
     override suspend fun getCoins(): List<CoinsData.Data> {
-
+        return listOf()
     }
 }
